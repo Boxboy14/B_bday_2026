26b5e6c8f5f6ac7d6d812af1b5833d07aff8939b
Rutu Kadam <80619938+Boxboy14@users.noreply.github.com>
2026-03-03 20:09:47 +0000

    remove error

--- script.js
+++ script.js
@@ -9,9 +9,6 @@ const photoUrls = [
 ];
   "Hey Babe, first of all I want wish you a very Happy Birthday. You turn 25 today. You are really getting old now 😂😂. I hope you have a fabulous day. This day is important to me as it is to you because if it was'nt for this day you would not have been in my life. Thank you for always sticking by my side even at my lowest. I promise to stay with you for atleast 50 more birthdays 😂. Once again A Happy Birthday to you ❤️😘. I love you ❤️❤️❤️.";
 
-// Replace these with your own photo file paths, for example: "images/photo1.jpg"
-const photoUrls = [];
-
 const revealBtn = document.getElementById("revealBtn");
 const cakePageBtn = document.getElementById("cakePageBtn");
 const cutCakeBtn = document.getElementById("cutCakeBtn");
